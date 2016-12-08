2bdc2401939bfd471ffa8e04d5d65091b95a2aa1
Marcus Kubota <marcus.kubota@gmail.com>
2016-12-07 21:30:17 -0500

    End of 5.4

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,9 +4,44 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   firstName: String,
   lastName: String,
-  email: String,
-  username: String,
-  password: String
+  email: {
+    type: String,
+    index: true
+  },
+  username: {
+    type: String,
+    trim: true,
+    unique: true
+  },
+  password: String,
+  create: {
+    type: Date,
+    default: Date.now
+  },
+  website: {
+    type: String,
+    get: function(url) {
+      if (!url) {
+        return url;
+      }
+      else {
+        if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+          url = 'http://' + url;
+        }
+        return url;
+      }
+    }
+  }
 });
+
+UserSchema.virtual('fullName').get(function() {
+  return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+  const splitName = fullName.split(' ');
+  this.firstName = splitName[0] || '';
+  this.lastName = splitName[1] || '';
+});
+
+UserSchema.set('toJSON', {getters: true, virtuals: true});
 
 mongoose.model('User', UserSchema);
